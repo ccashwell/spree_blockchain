a5ab69cc869925c5a3845d97b0bfe3a609b923b1
Chris Cashwell <ccashwell@gmail.com>
2015-07-31 12:01:59 -0500

    Change POST to GET for notification

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Spree::Core::Engine.routes.draw do
   get "/spree_blockchain/cancel", to: "blockchain#cancel", as: :spree_blockchain_cancel
   get "/spree_blockchain/instructions", to: "blockchain#instructions", as: :spree_blockchain_instructions
   get "/spree_blockchain/success", to: "blockchain#success", as: :spree_blockchain_success
-  post "/spree_blockchain/callback", to: "blockchain#callback", as: :spree_blockchain_callback
+  get "/spree_blockchain/callback", to: "blockchain#callback", as: :spree_blockchain_callback
 end
